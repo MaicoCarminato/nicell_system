cf78239e8289802017a7b0ffd4f24a46b17b5d9b
MaicoCarminato <88250807+MaicoCarminato@users.noreply.github.com>
2021-07-31 16:07:48 -0300

    Instalação

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,11 @@
+@extends('admin.layouts.app')
+
+@section('title', 'Criar novo post')
+
+@section('content')
+    <h1>Cadastrar novo post</h1>
+
+    <form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data">
+        @include('admin.posts._partials.form')
+    </form>
+@endsection
